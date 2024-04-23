afb171431718e72c2d21d1d0f7b629cef378a589
Romain Ilardi <romain.ilardi@gmail.com>
2024-04-23 10:12:28 +0200

    ajout package model et class Note

---
+++ app/src/main/java/com/openclassrooms/notes/model/Note.kt
@@ -0,0 +1,3 @@
+package com.openclassrooms.notes.model
+
+data class Note(var title: String, var content: String)
---
+++ app/src/main/java/com/openclassrooms/notes/widget/NoteViewHolder.kt
@@ -0,0 +1,22 @@
+package com.openclassrooms.notes.widget
+
+import androidx.recyclerview.widget.RecyclerView
+import com.openclassrooms.notes.databinding.NoteBinding
+import com.openclassrooms.notes.model.Note
+
+/**
+ * A view holder for displaying a note in a RecyclerView.
+ * @param binding The binding for the note layout.
+ */
+class NoteViewHolder(private val binding: NoteBinding): RecyclerView.ViewHolder(binding.root) {
+
+    /**
+     * Binds the view holder to a note.
+     * @param note The note to bind to the view holder.
+     */
+    fun bind(note: Note) {
+        binding.title.text = note.title
+        binding.body.text = note.content
+    }
+
+}
